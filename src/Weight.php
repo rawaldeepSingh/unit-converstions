c8908558f21bcd3776e2838a0090f3d3892ac5fb
rawaldeep <rawaldeep@chuck.be>
2020-10-14 14:35:54 +0000

    convert kilograms to lbs

---
+++ src/Weight.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Spatie\UnitConversions;
+
+class Weight
+{
+    public static function fromKilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
+
+    public function __construct(float $kilograms)
+    {
+        $this->kilograms = $kilograms;
+    }
+
+    public function toLbs(): float
+    {
+        return $this->kilograms * 2.204623;
+    }
+}
